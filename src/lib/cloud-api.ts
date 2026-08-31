c0a8c9a8b4dd410033b32194ff650809496ec5d5
Osuagwu101 <nnaemekasolomon31@gmail.com>
2026-08-31 08:47:24 +0100

    Correct signup response normalization source

--- src/lib/cloud-api.ts
+++ src/lib/cloud-api.ts
@@ -105,7 +105,7 @@ export async function activateInvitedStaffAccount(
         staff_invite_token: inviteToken,
       },
     }),
-  );
+  });
 }
 
 export async function refreshCloudSession(refreshToken: string) {
